3d4a2f8800395bf52227eee95414f7e20994cb42
Ramesh Shanmugam <reachrameshmobiledev@gmail.com>
2023-07-09 15:29:05 +0530

    Initial Commit

---
+++ PeerPay/PeerPay/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PeerPay
+//
+//  Created by Ramesh Shanmugam on 09/07/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PeerPay/PeerPay/PeerPayApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeerPayApp.swift
+//  PeerPay
+//
+//  Created by Ramesh Shanmugam on 09/07/23.
+//
+
+import SwiftUI
+
+@main
+struct PeerPayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
